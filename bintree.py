9671d98ecf418a8b3f852f81e74e9ddd71408dcc
zzremi <120573412+zzremi@users.noreply.github.com>
2022-12-18 16:19:46 +0100

    Add files via upload

--- bintree.py
+++ bintree.py
@@ -1,22 +1,20 @@
 '''
-Generate and draw binary tree
+Generate and draw binary tree, also solving this:
+https://leetcode.com/problems/path-sum-ii/
 '''
-
 
 
 import numpy as np
 
 
 class BinTreeNode:
-    def __init__(self, val = 0, left = None, right = None):
-        self.val = val
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
         self.left = left
         self.right = right
 
 		
 def generate_rand(nmax, left_weigth = 0, right_weigth = 0):
-
-    print(nmax)
 
     if (nmax > 0):
         r = np.random.normal(0,1)
@@ -34,7 +32,7 @@ def generate_rand(nmax, left_weigth = 0, right_weigth = 0):
 
 def draw(t, layer = 0, pref = ""):
 
-    print(layer,":",pref,"| val = ",t.val)
+    print(layer,":",pref,"| value = ",t.value)
     if (t.left != None):
         draw(t.left,layer+1,pref+"l")
     if (t.right != None):
@@ -44,7 +42,7 @@ def draw(t, layer = 0, pref = ""):
 def form_str(t, s, nl, pos, num, layer = 0):
 
     if (layer == num):
-        return s[0:pos] + str(t.val) + s[pos+1:]
+        return s[0:pos] + str(t.value) + s[pos+1:]
     else:
         if (t.left != None):
             s = form_str(t.left,s,nl,pos-2**(nl-layer-1),num,layer+1)
@@ -68,22 +66,43 @@ def get_number_of_layers(t, num = 0):
     return max(num_left,num_right)
 	
 	
+def form_sum_dict(t, d, pref='h', sum=0):
+
+	sum += t.value
+	if ((t.left == None) and (t.right == None)):
+		d[pref] = sum
+	else:
+		if (t.left != None):
+			form_sum_dict(t.left, d, pref+'l', sum)
+		if (t.right != None):
+			form_sum_dict(t.right, d, pref+'r', sum)	
+			
+	
 if __name__ == "__main__":
     
-    n = 8
+	n = 8
+	target_sum  = 0
 	
-    t = generate_rand(n)
-    draw(t)
-    nl = get_number_of_layers(t)
-    print(nl)
-    for i in range(0,nl):
-        s = ""
-        for j in range(0,2**(nl+1)):
-            s = s + "_"
-        print(i,":",form_str(t, s, nl, 2**nl, i))
+	t = generate_rand(n)
+	draw(t)
+	nl = get_number_of_layers(t)
+	print('\n')
+	for i in range(0,nl+1):
+		s = ""
+		for j in range(0,2**(nl+1)):
+			s = s + "_"
+		print(i,":",form_str(t, s, nl, 2**nl, i))
 		
-		
-		
-		
-		
-		
+	d = {}
+	form_sum_dict(t,d)
+	print('\n')
+	[print(k,'|',d[k]) for k in d]
+	print('\n')
+	klist = list(d.keys())
+	vlist = list(d.values())	
+	if (target_sum in vlist):
+		print(klist[vlist.index(target_sum)])
+	else:
+		print('None')
+	
+	
